7b8d945d440ce1c21f12746e15918440f9505539
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 11:48:39 -0800

    Add route for optional query params and extracting them.

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(private route: ActivatedRoute, private service: GithubFollowersService) { }
 
   ngOnInit() {
+    // Use this to get the required params in the subscribe method
+    this.route.paramMap.subscribe();
+    // Use this to get the required params if you're going to stay in the same component
+    let id = this.route.snapshot.paramMap.get('id');
+
+    // Use this to get the optional params 
+    this.route.queryParamMap.subscribe();
+    let page = this.route.snapshot.queryParamMap.get('page');
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
